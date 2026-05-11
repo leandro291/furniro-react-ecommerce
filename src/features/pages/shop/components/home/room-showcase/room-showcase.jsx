1f17ef44b1d26832e071942f65f91ab997ca7d42
leandro291 <lenadrito1@gmail.com>
2026-05-11 16:46:31 -0500

    fix: fix the border gray for the footer

--- src/features/pages/shop/components/home/room-showcase/room-showcase.jsx
+++ src/features/pages/shop/components/home/room-showcase/room-showcase.jsx
@@ -3,7 +3,7 @@ import { RoomShowcaseMosaic } from './room-showcase-mosaic'
 
 export function RoomShowcase() {
     return (
-        <section className='container mx-auto mt-15'>
+        <section className='container mx-auto my-15'>
             <h3 className='text-center font-["Poppins"] text-[20px] font-semibold text-gray-600'>Share your setup with</h3>
             <h2 className='text-center font-["Poppins"] text-[40px] font-bold text-gray-950'>#FuniroFurniture</h2>
             <RoomShowcaseMosaic />
