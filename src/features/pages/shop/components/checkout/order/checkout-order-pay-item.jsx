ee78d9cccb5fe99d6774bba99853492a3dbbbdd0
leandro291 <lenadrito1@gmail.com>
2026-05-11 17:17:14 -0500

    fix: fix the width for the list price of products

--- src/features/pages/shop/components/checkout/order/checkout-order-pay-item.jsx
+++ src/features/pages/shop/components/checkout/order/checkout-order-pay-item.jsx
@@ -4,7 +4,7 @@ export function CheckoutOrderPayItem({ id, title, quantity, pricePerProduct }) {
 
     return (
         <div className='flex justify-between'>
-            <span className='font-["Poppins"] font-medium text-gray-500/80 w-full max-w-60 sm:max-w-150'>{title} x {quantity}</span>
+            <span className='font-["Poppins"] font-medium text-gray-500/80 w-45 lg:w-70 sm:w-110'>{title} x {quantity}</span>
             <span className='font-["Poppins"] font-light '>Rs {pricePerProduct(id)}</span>
         </div>
     )
