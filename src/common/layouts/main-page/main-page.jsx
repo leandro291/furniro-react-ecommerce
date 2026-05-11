e18093740b5e466f8e99d1e8af8d454a3ada34b2
leandro291 <lenadrito1@gmail.com>
2026-05-10 21:57:51 -0500

    feat(layout): create MainPage wrapper integrating persistent navbar and footer

---
+++ src/common/layouts/main-page/main-page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Navbar } from '../../components/navbar/navbar'
+import { Footer } from '../../components/footer/footer'
+import { Outlet } from 'react-router-dom'
+
+export function MainPage() {
+
+    return (
+        <div className='w-full min-h-screen grid grid-rows-[100px_1fr_500px] bg-slate-50'>
+            <Navbar />
+
+            <main className='w-full min-h-screen container mx-auto bg-white'>
+                <Outlet />
+            </main>
+
+            <Footer />
+        </div>
+
+    )
+}
