0f08461804de57082ee7f99b88f26057b12c9330
lenadro291 <lenadrito1@gmail.com>
2026-08-25 20:44:12 -0500

    fix: ensure logout function clears user state in auth store

--- src/features/pages/shop/store/auth-store.js
+++ src/features/pages/shop/store/auth-store.js
@@ -9,7 +9,7 @@ export const useAuthStore = create (
 
             setUser: (user) => set({ user }),
             setToken: (token) => set({ token }),
-            logout : () => set({ token: null}),
+            logout : () => set({ token: null, user: null }),
         }),
         
         {
